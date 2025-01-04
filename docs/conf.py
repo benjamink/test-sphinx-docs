9a7717cad9d60ef080ebbbab9655f159eaf363cb
Benjamin Krein <superbenk@gmail.com>
2025-01-04 22:18:55 +0000

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'groundwork'
+html_theme = 'sphinx_pdj_theme'
 #html_theme_options = {
 #  'logo': 'FujiNet-Header.png',
 #  'bgcolor': '#302B35',
